43237f395b9da7d5848ab765f358d10eab71070d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-06-25 21:34:35 -0400

    Fixes

--- sinovar/embedding/__init__.py
+++ sinovar/embedding/__init__.py
@@ -1,5 +1,5 @@
-from diffusion_map import compute_diffusion_embedding
-from affinity import (
+from .diffusion_map import compute_diffusion_embedding
+from .affinity import (
     knn_affinity_from_squared_distance_matrix, 
     adaptive_sigma2_median
 )
