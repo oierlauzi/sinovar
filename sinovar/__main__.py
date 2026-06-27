943e71f7941478ae8e6996f1790abf50431776e1
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-06-27 13:01:58 -0400

    Reduced kNN sigma

--- sinovar/__main__.py
+++ sinovar/__main__.py
@@ -188,7 +188,7 @@ def run(args: argparse.Namespace):
         mmap_distances2.flush()
 
     logger.info('Computing affinity matrix from distances')
-    sigma2 = embedding.adaptive_sigma2_median(distances2, k=32)
+    sigma2 = embedding.adaptive_sigma2_median(distances2, k=16)
     affinity = embedding.knn_affinity_from_squared_distance_matrix(
         distances2,
         k=4096,
